f8ba168ca62992c20e07450d11e349d61e09be5b
Alfredo <alfredonzk@gmail.com>
2022-08-02 09:29:56 -0300

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
+import { Header } from "./Components/Header"
 import { Post } from "./Post"
+
+import styles from './App.module.css'
+
+import './global.css'
+import { Sidebar } from "./Components/Sidebar"
 
 export function App() {
   return (
     <div>
-      <Post
-        author="Alfredo Pamplona"
-        content="Lorem dsyhas asunebsh asknas ebksnh"
-      />
-      <Post
-        author="Diego Fernandez"
-        content="Novo post de diego fernandez"
-      />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Alfredo Pamplona"
+            content="Lorem dsyhas asunebsh asknas ebksnh"
+          />
+          <Post
+            author="Diego Fernandez"
+            content="Novo post de diego fernandez"
+          />
+        </main>
+      </div>
     </div>
   )
 }
